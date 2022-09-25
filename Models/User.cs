56f5918e4913ebe03a760711e3186ab29932f1c6
Lenovo <Lenovo@DESKTOP-4BICV1P>
2022-09-25 22:44:48 +0200

    Add project files.

---
+++ Models/User.cs
@@ -0,0 +1,18 @@
+﻿namespace Roadmap.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string UserGroup { get; set; }
+        public string EmployeeType { get; set; }
+        public string Level { get; set; }
+        public string Reviewer { get; set; }
+        public bool Active { get; set; }
+        
+        public User()
+        {
+
+        }
+    }
+}
